7c7b5def9223ec1221c50f8b314199a3c094a1c6
jwcorle <jcorley.w@gmail.com>
2022-11-25 14:36:09 -0500

    feat(website): initial

---
+++ src/website/src/Home.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './Home.module.sass';
+
+const Home = () => {
+  return (
+    <div className="section1">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
